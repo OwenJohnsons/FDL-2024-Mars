6ef28ed1c72818d465b8385c5a3ab0e72201c1b2
K P <kanakparmar06@gmail.com>
2024-07-18 20:04:46 +0000

    Upload New File

---
+++ ML_Models/LSTM/model.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch
+
+class LSTM(nn.Module):
+    def __init__(self, lstm_features, lstm_hidden_units, output_dim, num_layers_lstm):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=lstm_features, hidden_size=lstm_hidden_units,
+                            batch_first=True,
+                            num_layers=num_layers_lstm)
+        self.fc = nn.Linear(lstm_hidden_units, output_dim)
+        self.softmax = nn.Softmax(dim=0) #dont change
+
+        self.num_layers = num_layers_lstm
+        self.hidden_units = lstm_hidden_units
+
+    def forward(self, batch):
+        x = batch
+
+        # re-initialize hidden and cell states to make it a stateless model
+        batch_size = len(batch)
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+
+        out, _ = self.lstm(x.float(), (h0,c0))
+        out = self.fc(out[:,-1])
+
+        # using cross entropy loss automatically implies a softmax computation on output so we dont need to do it
+        # out = self.softmax(out)
+
+        return out
