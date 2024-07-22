8ee3de7ea7f323a0f65e149a726048fd9b838af2
Owen Johnson <ojohnson@tcd.ie>
2024-07-22 16:58:28 -0700

    GCP query script, update.

--- bash-scripts/gcp-vm-query.sh
+++ bash-scripts/gcp-vm-query.sh
@@ -9,34 +9,37 @@ fi
 
 # Constants
 VM_COUNT=4
-VM_NAME_PREFIX="SAMurAI-Training-VM"
-IMAGE_FAMILY="pytorch-latest-gpu"
-IMAGE_PROJECT="deeplearning-platform-release"
-ADD_GPUS="yes"
-GPU_TYPES=("nvidia-tesla-a100" "nvidia-tesla-h100" "nvidia-tesla-b100")
-GPU_COUNT=2
-DISK_SIZE="500GB" 
+VM_NAME_PREFIX="samurai-training-vm" # Corrected to follow the naming conventions
+IMAGE_FAMILY="pytorch-latest-gpu" # PyTorch image family
+IMAGE_PROJECT="deeplearning-platform-release" # PyTorch image project
+DISK_SIZE="500GB" # Disk size
+
+# List of machine types
+MACHINE_TYPES=("a3-highgpu-8g" "a3-megagpu-8g" "a2-highgpu-1g" "a2-highgpu-2g" "a2-highgpu-4g" "a2-highgpu-8g" "a2-megagpu-1g" "a2-megagpu-2g" "a2-megagpu-4g" "a2-megagpu-8g")
 
 # List of US zones
 US_ZONES=("us-central1-a" "us-central1-b" "us-central1-c" "us-central1-f" "us-east1-b" "us-east1-c" "us-east1-d" "us-east4-a" "us-east4-b" "us-east4-c" "us-west1-a" "us-west1-b" "us-west1-c" "us-west2-a" "us-west2-b" "us-west2-c")
 
+# Function to create a VM
 create_vm() {
     local vm_name=$1
     local zone=$2
-    local gpu_flag=$3
+    local machine_type=$3
 
-    echo "Creating VM: $vm_name in zone: $zone with GPU flag: $gpu_flag"
+    echo "Creating VM: $vm_name in zone: $zone with machine type: $machine_type and disk size: $DISK_SIZE"
 
     gcloud compute instances create $vm_name \
+        --machine-type=$machine_type \
         --zone=$zone \
         --image-family=$IMAGE_FAMILY \
         --image-project=$IMAGE_PROJECT \
         --boot-disk-size=$DISK_SIZE \
-        $gpu_flag
+        --maintenance-policy=TERMINATE
 
     return $?
 }
 
+# Loop to create 4 VMs successfully
 success_count=0
 attempted_count=0
 
@@ -44,24 +47,12 @@ while [ $success_count -lt $VM_COUNT ]
 do
     vm_name="${VM_NAME_PREFIX}-${attempted_count}"
     zone=${US_ZONES[$RANDOM % ${#US_ZONES[@]}]}
+    machine_type=${MACHINE_TYPES[$RANDOM % ${#MACHINE_TYPES[@]}]}
 
-    if [[ "$ADD_GPUS" == "yes" ]]; then
-        for gpu_type in "${GPU_TYPES[@]}"
-        do
-            gpu_flag="--accelerator type=$gpu_type,count=$GPU_COUNT"
-            create_vm $vm_name $zone "$gpu_flag"
-            if [ $? -eq 0 ]; then
-                echo "VM $vm_name created successfully."
-                ((success_count++))
-                break
-            fi
-        done
-    else
-        create_vm $vm_name $zone ""
-        if [ $? -eq 0 ]; then
-            echo "VM $vm_name created successfully."
-            ((success_count++))
-        fi
+    create_vm $vm_name $zone "$machine_type"
+    if [ $? -eq 0 ]; then
+        echo "VM $vm_name created successfully in zone $zone."
+        ((success_count++))
     fi
 
     ((attempted_count++))
