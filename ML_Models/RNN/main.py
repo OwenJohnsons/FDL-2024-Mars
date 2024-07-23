58fdcf5d6cc796376dcf7e7f213966acfa217272
K P <kanakparmar06@gmail.com>
2024-07-23 02:31:05 +0000

    Update main.py

--- ML_Models/RNN/main.py
+++ ML_Models/RNN/main.py
@@ -130,8 +130,8 @@ for epoch in range(num_epochs):
 
             # write to tensorboard, print epoch loss to console
             batch_global_val += data_record_interval
-            writer.add_scalar('Batch Val Loss', loss_train, batch_global_val)
-            writer.add_scalar('Batch Val Accuracy', batch_accuracy_train, batch_global_val)
+            writer.add_scalar('Batch Val Loss', loss_val, batch_global_val)
+            writer.add_scalar('Batch Val Accuracy', batch_accuracy_val, batch_global_val)
 
             
             # delete pred and losses to reduce memory consumption>
