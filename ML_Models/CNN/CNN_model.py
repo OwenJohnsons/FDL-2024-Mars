459b074f8246cefe6854614ea646c15dbade0c23
Owen Johnson <ojohnson@tcd.ie>
2024-07-24 23:49:43 +0000

    Delete CNN_model.py

--- ML_Models/CNN/CNN_model.py
+++
@@ -1,5 +0,0 @@
-'''
-Code Authors: Owen A. Johnson and Arushi Saxena 
-Date: 22/07/2024
-Code Purpose: This code utlizes pytorch to create a 3D CNN model to analyse .mp4 videos created from Mass Spectrometry Data. 
-'''
