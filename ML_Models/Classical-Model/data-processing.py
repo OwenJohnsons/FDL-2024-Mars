e5c856a8fc6b229f69dd460ff0ab4ad79116482a
Owen Johnson <ojohnson@tcd.ie>
2024-07-25 10:20:36 -0700

    Adding more empty files for the structure.

---
+++ ML_Models/Classical-Model/data-processing.py
@@ -0,0 +1,19 @@
+'''
+Code Purpose: 
+Author: Owen A. Johnson
+Last updated:
+'''
+
+import numpy as np 
+
+def load_data():
+    """
+    Function Purpose: Load the data
+
+    Args:
+        None
+
+    Returns:
+    """
+
+    return 
--- ML_Models/Classical-Model/main.py
+++ ML_Models/Classical-Model/main.py
@@ -4,6 +4,9 @@ Code Purpose:
 Modules:
 
 Note:
+    - data-processing.py is a module that contains functions for data processing and loading
+    - model.py is a module that contains functions for model building and training
+    - train-evaluate.py is a module that contains functions for training and evaluating the model
   
 Imports:
 
